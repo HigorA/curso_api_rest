6a951e42e5d60365af6b10f88e23a736d0bc7de7
Higor <higor.a.santos12@hotmail.com>
2023-02-18 10:18:02 -0300

    Seção 15 Implementação Swagger

---
+++ src/main/java/br/com/curso/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package br.com.curso.config;
+
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title("RESTFUL API Java 18 - Spring boot 3")
+                .version("v1")
+                .description("API desenvolvida no curso de APIs Restful")
+                .termsOfService("")
+                .license(new License().name("Apache 2.0").url(""))
+        );
+    }
+}
